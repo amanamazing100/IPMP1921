494ea059c284e8996d82f15fc20df3cc614afe51
Aman Anand <53084393+amanamazing100@users.noreply.github.com>
2021-05-28 11:44:37 +0530

    Create pow(x, n).cpp

---
+++ week6/dnc/pdf/one go/pow(x, n).cpp
@@ -0,0 +1,9 @@
+double power(double x, unsigned int y)
+{
+    if (y == 0)
+        return 1;
+    else if (y % 2 == 0)
+        return power(x, y / 2) * power(x, y / 2);
+    else
+        return x * power(x, y / 2) * power(x, y / 2);
+}
