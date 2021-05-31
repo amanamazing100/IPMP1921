4322aa476851ea790f6a70018fcb77d9631634fa
Aman Anand <53084393+amanamazing100@users.noreply.github.com>
2021-05-31 11:43:16 +0530

    Create populate inorder successor.cpp

---
+++ Week 7/trees/one go/populate inorder successor.cpp
@@ -0,0 +1,11 @@
+void populateNext(node* p)
+{
+    static node *next = NULL;
+    if (p)
+    {
+        populateNext(p->right);
+  p->next = next;
+        next = p;
+        populateNext(p->left);
+    }
+}
