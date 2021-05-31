f488606e8bef15d251e82f41a602f4ab58e90e26
Aman Anand <53084393+amanamazing100@users.noreply.github.com>
2021-05-31 11:25:59 +0530

    Create inorder preorder postorder.cpp

---
+++ Week 7/trees/one go/inorder preorder postorder.cpp
@@ -0,0 +1,27 @@
+void printInorder(struct Node* node)
+{
+    if (node == NULL)
+        return;
+    printInorder(node->left);
+    cout << node->data << " ";
+    printInorder(node->right);
+}
+
+void printPreorder(struct Node* node)
+{
+    if (node == NULL)
+        return;
+    cout << node->data << " ";
+    printPreorder(node->left);
+    printPreorder(node->right);
+}
+
+void printPostorder(struct Node* node)
+{
+    if (node == NULL)
+        return;
+    printPostorder(node->left);
+    printPostorder(node->right);
+    cout << node->data << " ";
+}
+ 
