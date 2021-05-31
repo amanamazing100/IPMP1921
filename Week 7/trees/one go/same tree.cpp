ffe18be1da5895598321d0f1628f124d322a6a25
Aman Anand <53084393+amanamazing100@users.noreply.github.com>
2021-05-31 11:23:16 +0530

    Create same tree.cpp

---
+++ Week 7/trees/one go/same tree.cpp
@@ -0,0 +1,14 @@
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL && q==NULL)
+            return 1;
+        if(p!=NULL && q!=NULL){
+            if(p->val!=q->val)
+                return 0;
+            return isSameTree(p->left, q->left)&&isSameTree(p->right, q->right);
+        }
+        return 0;
+    }
+};
