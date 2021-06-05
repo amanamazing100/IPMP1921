59e1935c748ecd8dfaeb6885d93778df9f01fb71
Aman Anand <53084393+amanamazing100@users.noreply.github.com>
2021-06-05 14:39:41 +0530

    Create Construct Full Binary Tree from given preorder and postorder traversals.cpp

---
+++ Week 7/trees/review/Construct Full Binary Tree from given preorder and postorder traversals.cpp
@@ -0,0 +1,29 @@
+node* constructTreeUtil (int pre[], int post[], int* preIndex,
+								int l, int h, int size)
+{
+	if (*preIndex >= size || l > h)
+		return NULL;
+	node* root = newNode ( pre[*preIndex] );
+	++*preIndex;
+	if (l == h)
+		return root;
+	int i;
+	for (i = l; i <= h; ++i)
+		if (pre[*preIndex] == post[i])
+			break;
+	if (i <= h)
+	{
+		root->left = constructTreeUtil (pre, post, preIndex,
+												l, i, size);
+		root->right = constructTreeUtil (pre, post, preIndex,
+												i + 1, h, size);
+	}
+
+	return root;
+}
+
+node *constructTree (int pre[], int post[], int size)
+{
+	int preIndex = 0;
+	return constructTreeUtil (pre, post, &preIndex, 0, size - 1, size);
+}
